b2b9e61a9f9faef2cf3e07ff485a3545d81c2a8f
Hasham <74524735+Hasham99@users.noreply.github.com>
2024-11-26 14:44:39 +0500

    backend basic setup

---
+++ src/app.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+
+}))
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser())
+app.use(morgan('dev'))
+
+
+//routes import
+import userRouter from "./routes/user.routes.js";
+import morgan from "morgan";
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
+
+// app.use("/", (req, res) => {
+//     res.send("working")
+// })
+
+// http://localhost:8090//api/v1/users/register
+
+
+
+export { app }
